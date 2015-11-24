1b5f330c0f788deadc8c1e2849cd16d08566ae44
Hyunbok Lee <hyunbogi@gmail.com>
2015-11-24 20:16:27 +0900

    OrderRepository 커스텀 구현 추가

--- src/main/java/com/hyunbogi/jpaweb/repository/custom/OrderRepositoryImpl.java
+++ src/main/java/com/hyunbogi/jpaweb/repository/custom/OrderRepositoryImpl.java
@@ -1,8 +1,12 @@
 package com.hyunbogi.jpaweb.repository.custom;
 
 import com.hyunbogi.jpaweb.domain.Order;
+import com.hyunbogi.jpaweb.domain.QMember;
+import com.hyunbogi.jpaweb.domain.QOrder;
 import com.hyunbogi.jpaweb.domain.search.OrderSearch;
+import com.mysema.query.jpa.JPQLQuery;
 import org.springframework.data.jpa.repository.support.QueryDslRepositorySupport;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -13,6 +17,20 @@ public class OrderRepositoryImpl extends QueryDslRepositorySupport implements Cu
 
     @Override
     public List<Order> search(OrderSearch orderSearch) {
-        return null;
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        JPQLQuery query = from(order);
+
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query.leftJoin(order.member, member)
+                    .where(member.name.contains(orderSearch.getMemberName()));
+        }
+
+        if (orderSearch.getOrderStatus() != null) {
+            query.where(order.status.eq(orderSearch.getOrderStatus()));
+        }
+
+        return query.list(order);
     }
 }
